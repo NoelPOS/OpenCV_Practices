75811f9d783c7e29fcea245997d97b3459bf0d1a
Noel Paing Oak Soe <u6530183@au.edu>
2024-06-21 11:44:10 +0700

    Week 3 Lab

---
+++ ImageProcessing/medianblur.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('original2.jpg')
+
+
+median = cv2.medianBlur(src=image, ksize=5)
+ 
+cv2.imshow('Original', image)
+cv2.imshow('Median Blurred', median)
+     
+cv2.waitKey()
+cv2.imwrite('median_blur.jpg', median)
+cv2.destroyAllWindows()
---
+++ ImageProcessing/testing.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+ 
+image = cv2.imread('original.jpg')
+ 
+# Print error message if image is null
+if image is None:
+    print('Could not read image')
+ 
+# Apply identity kernel
+kernel1 = np.array([[0, 0, 0],
+                    [0, 1, 0],
+                    [0, 0, 0]])
+ 
+identity = cv2.filter2D(src=image, ddepth=-1, kernel=kernel1)
+ 
+cv2.imshow('Original', image)
+cv2.imshow('Identity', identity)
+     
+cv2.waitKey()
+cv2.imwrite('identity.jpg', identity)
+cv2.destroyAllWindows()
+ 
+# Apply blurring kernel
+kernel2 = np.ones((5, 5), np.float32) / 25
+img = cv2.filter2D(src=image, ddepth=-1, kernel=kernel2)
+ 
+cv2.imshow('Original', image)
+cv2.imshow('Kernel Blur', img)
+     
+cv2.waitKey()
+cv2.imwrite('blur_kernel.jpg', img)
+cv2.destroyAllWindows()
