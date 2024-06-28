b744ace5a15c7fdb647991bb1bfa6dcc18fb5b17
Noel Paing Oak Soe <u6530183@au.edu>
2024-06-28 20:21:53 +0700

    Class 4 FFT practice

--- ImageProcessing/Class Exercises/medianblur.py
+++ ImageProcessing/Class Exercises/medianblur.py
@@ -9,6 +9,6 @@ median = cv2.medianBlur(src=image, ksize=5)
 cv2.imshow('Original', image)
 cv2.imshow('Median Blurred', median)
      
-cv2.waitKey()
-cv2.imwrite('median_blur.jpg', median)
+cv2.waitKey(5000)
+# cv2.imwrite('median_blur.jpg', median)
 cv2.destroyAllWindows()
